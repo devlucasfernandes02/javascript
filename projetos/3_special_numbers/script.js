244fb890396c85131f50fd5ceb80094d49e84fc3
Lucas-Fernandes02 <alfds10@hotmail.com>
2024-05-27 15:30:05 -0300

    tipos de dados

--- projetos/3_special_numbers/script.js
+++ projetos/3_special_numbers/script.js
@@ -1,4 +1,6 @@
+console.log(typeof 'Oi, meu nome é Lucas');
+console.log(typeof 'Tenho 22 anos');
+console.log(typeof `Teste`);
+console.log(typeof 'Infinity');
 console.log(typeof Infinity);
-console.log(typeof -Infinity);
-console.log(typeof NaN);
 
---
+++ projetos/5_strings_avancadas/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha\nSegunda linha");
+console.log("Ela se chama 'Alice'");
+console.log("Meu nome é 'Lucas'");
+console.log(`A Multiplicação de 5 por 3 é ${5*3}`);
+console.log("O " + "meu " + "nome "+ "é " +"Lucas")
